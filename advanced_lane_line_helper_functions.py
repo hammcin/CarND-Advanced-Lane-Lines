3e65da3c3c71c76fdd5d9cbf3756d5e617f91d72
Hamadi McIntosh <hamadi.mc@sbcglobal.net>
2019-03-20 14:35:39 -0400

    feat: Create threshold binary image

--- advanced_lane_line_helper_functions.py
+++ advanced_lane_line_helper_functions.py
@@ -29,3 +29,129 @@ def find_cal_pts(images, nx=9, ny=6):
             objpoints.append(objp)
 
     return imgpoints, objpoints
+
+# Function that applies Sobel x or y,
+# then takes an absolute value and applies a threshold.
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    elif orient == 'y':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    # 3) Take the absolute value of the derivative or gradient
+    abs_sobel = np.absolute(sobel)
+
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+
+    # 5) Create a mask of 1's where the scaled gradient magnitude
+            # is > thresh_min and < thresh_max
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+# Function that applies Sobel x and y,
+# then computes the magnitude of the gradient
+# and applies a threshold
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    # 3) Calculate the magnitude
+    mag = np.sqrt(sobelx**2 + sobely**2)
+
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scaled_mag = np.uint8(255*mag/np.max(mag))
+
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(scaled_mag)
+    binary_output[(scaled_mag >= mag_thresh[0]) & (scaled_mag <= mag_thresh[1])] = 1
+
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+# Function that applies Sobel x and y,
+# then computes the direction of the gradient
+# and applies a threshold.
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    # 3) Take the absolute value of the x and y gradients
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    grad_orient = np.arctan2(abs_sobely, abs_sobelx)
+
+    # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(grad_orient)
+    binary_output[(grad_orient >= thresh[0]) & (grad_orient <= thresh[1])] = 1
+
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+# Function that thresholds the S-channel of HLS
+# Uses exclusive lower bound (>) and inclusive upper (<=)
+def hls_select(img, thresh=(0, 255)):
+
+    # 1) Convert to HLS color space
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    S = hls[:,:,2]
+
+    # 2) Apply a threshold to the S channel
+    binary_output = np.zeros_like(S)
+    binary_output[(S > thresh[0]) & (S <= thresh[1])] = 1
+
+    # 3) Return a binary image of threshold result
+    return binary_output
+
+# Pipeline
+def pipeline(img, s_thresh = (215, 240), sobelx_thresh=(20, 100), r_thresh = (225, 255), ksize=3):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:,:,2]
+
+    r_channel = img[:,:,0]
+
+    # Apply each of the thresholding functions
+    gradx = abs_sobel_thresh(img, orient='x', sobel_kernel=ksize, thresh=sobelx_thresh)
+
+    combined = np.zeros_like(gradx)
+    combined[(gradx == 1)] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
+    r_binary = np.zeros_like(r_channel)
+    r_binary[(r_channel > r_thresh[0]) & (r_channel <= r_thresh[1])] = 1
+
+    color_combined = np.zeros_like(s_binary)
+    color_combined[(s_binary == 1) | (r_binary == 1)] = 1
+
+    # Stack each channel
+    # color_binary = np.dstack(( np.zeros_like(combined), combined, s_binary)) * 255
+
+    color_binary = np.zeros_like(combined)
+    color_binary[(combined == 1) | (color_combined == 1)] = 1
+
+    return color_binary
